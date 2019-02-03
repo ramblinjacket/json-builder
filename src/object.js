dc0cf72b9999f4ce13634dd35884791e560bf1eb
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2019-02-03 06:55:09 -0500

    got basic structure created

---
+++ src/object.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Objects extends Component {
+  render() {
+    return (
+      <div>
+          <p>&#123;</p>
+          <button>Add Field</button>
+          <p>&#125;</p>
+      </div>
+    );
+  }
+}
